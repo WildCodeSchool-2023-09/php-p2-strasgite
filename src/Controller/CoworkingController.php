ebdd4c6616a0b0635dd2a29de39990069bb50bba
antoninbrdl24 <antonin.brondel24@gmail.com>
2023-10-31 09:48:06 +0100

    ajout de la branche coworking , architecture

--- src/Controller/CoworkingController.php
+++ src/Controller/CoworkingController.php
@@ -4,9 +4,6 @@ namespace App\Controller;
 
 class CoworkingController extends AbstractController
 {
-    /**
-     * Display home page
-     */
     public function index(): string
     {
         return $this->twig->render('Coworking/index.html.twig');
