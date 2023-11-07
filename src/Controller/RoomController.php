ed6bf642e89af8440380f6fce1b455090bbc0780
StyMey <stephanie2510@gmail.com>
2023-11-07 11:55:40 +0100

    add fist room

---
+++ src/Controller/RoomController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Controller;
+
+class RoomController extends AbstractController
+{
+    public function show(): string
+    {
+    return $this->twig->render('chambre/show.html.twig');
+    }
+}
