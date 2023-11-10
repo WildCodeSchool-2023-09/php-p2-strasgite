85bd69437e97af1b995f024a62d25069a997b5af
Yazzz28 <yacinemennaa@gmail.com>
2023-11-10 09:22:40 +0100

    ajustements

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -8,12 +8,6 @@ class DashboardController extends AbstractController
 {
     public function index()
     {
-        /*if (isset($_SESSION['isadmin']) && $_SESSION['isadmin'] === true) {
-            return $this->twig->render('admin/dashboard/index.html.twig');
-        } else {
-            header('Location:/');
-        }*/
-
         if (!isset($_SESSION['isadmin']) || $_SESSION['isadmin'] === 0 || !isset($_SESSION['islogin'])) {
             header('Location:/');
         } else {
