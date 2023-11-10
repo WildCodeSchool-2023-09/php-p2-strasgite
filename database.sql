9a76944ad109eee58a319e9ffa07a2f644af0cf1
StyMey <stephanie2510@gmail.com>
2023-11-10 11:09:47 +0100

    first commit admin-room

--- database.sql
+++ database.sql
@@ -1,63 +1,101 @@
--- phpMyAdmin SQL Dump
--- version 4.5.4.1deb2ubuntu2
--- http://www.phpmyadmin.net
---
--- Client :  localhost
--- Généré le :  Jeu 26 Octobre 2017 à 13:53
--- Version du serveur :  5.7.19-0ubuntu0.16.04.1
--- Version de PHP :  7.0.22-0ubuntu0.16.04.1
+-- Active: 1698143962948@@127.0.0.1@3306
+-- MySQL Workbench Forward Engineering
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 
+SET
+    @OLD_FOREIGN_KEY_CHECKS = @ @FOREIGN_KEY_CHECKS,
+    FOREIGN_KEY_CHECKS = 0;
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+SET
+    @OLD_SQL_MODE = @ @SQL_MODE,
+    SQL_MODE = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
---
--- Base de données :  `simple-mvc`
---
+-- -----------------------------------------------------
 
--- --------------------------------------------------------
+-- Schema strasgite
 
---
--- Structure de la table `item`
---
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS strasgite DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+-- -----------------------------------------------------
 
-CREATE TABLE `item` (
-  `id` int(11) UNSIGNED NOT NULL,
-  `title` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+-- Schema strasgite
 
---
--- Contenu de la table `item`
---
+-- -----------------------------------------------------
 
-INSERT INTO `item` (`id`, `title`) VALUES
-(1, 'Stuff'),
-(2, 'Doodads');
+USE `strasgite` ;
 
---
--- Index pour les tables exportées
---
+-- -----------------------------------------------------
 
---
--- Index pour la table `item`
---
-ALTER TABLE `item`
-  ADD PRIMARY KEY (`id`);
+-- Table `strasgite`.`image`
 
---
--- AUTO_INCREMENT pour les tables exportées
---
+-- -----------------------------------------------------
 
---
--- AUTO_INCREMENT pour la table `item`
---
-ALTER TABLE `item`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+CREATE TABLE
+    IF NOT EXISTS `strasgite`.`image` (
+        `id_image` INT NOT NULL AUTO_INCREMENT,
+        `img` TEXT NOT NULL,
+        `name` VARCHAR(45) NOT NULL,
+        `status` VARCHAR(45) NULL,
+        PRIMARY KEY (`id_image`)
+    ) ENGINE = InnoDB;
+
+-- -----------------------------------------------------
+
+-- Table `strasgite`.`options`
+
+-- -----------------------------------------------------
+
+CREATE TABLE
+    IF NOT EXISTS `strasgite`.`options` (
+        `id_options` INT NOT NULL AUTO_INCREMENT,
+        `name` VARCHAR(45) NOT NULL,
+        `is_available` TINYINT NOT NULL,
+        `prix` FLOAT NOT NULL,
+        PRIMARY KEY (`id_options`)
+    ) ENGINE = InnoDB;
+
+-- -----------------------------------------------------
+
+-- Table `strasgite`.`categories`
+
+-- -----------------------------------------------------
+
+CREATE TABLE
+    IF NOT EXISTS `strasgite`.`categories` (
+        `id_categories` INT NOT NULL AUTO_INCREMENT,
+        `nb_personnes` INT NOT NULL,
+        `taille` INT NOT NULL,
+        `theme` VARCHAR(45) NOT NULL,
+        PRIMARY KEY (`id_categories`)
+    ) ENGINE = InnoDB;
+
+-- -----------------------------------------------------
+
+-- Table `strasgite`.`chambre`
+
+-- -----------------------------------------------------
+
+CREATE TABLE
+    IF NOT EXISTS `strasgite`.`chambre` (
+        `id_chambre` INT NOT NULL AUTO_INCREMENT,
+        `name` VARCHAR(45) NOT NULL,
+        `id_option` INT NULL,
+        `is_available` TINYINT NOT NULL,
+        `id_categorie` INT NOT NULL,
+        `prix` FLOAT NOT NULL,
+        `id_image` INT NOT NULL,
+        PRIMARY KEY (`id_chambre`),
+        INDEX `id_image_idx` (`id_image` ASC) VISIBLE,
+        INDEX `id_option_idx` (`id_option` ASC) VISIBLE,
+        INDEX `id_categorie_idx` (`id_categorie` ASC) VISIBLE,
+        CONSTRAINT `id_image` FOREIGN KEY (`id_image`) REFERENCES `strasgite`.`image` (`id_image`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+        CONSTRAINT `id_option` FOREIGN KEY (`id_option`) REFERENCES `strasgite`.`options` (`id_options`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+        CONSTRAINT `id_categorie` FOREIGN KEY (`id_categorie`) REFERENCES `strasgite`.`categories` (`id_categories`) ON DELETE NO ACTION ON UPDATE NO ACTION
+    ) ENGINE = InnoDB;
+
+-- -----------------------------------------------------
+
+-- Table `strasgite`.`user`
+
+... (99 lignes restantes)
